b3b4ba84cf4cbef9f8bec28a6a2eb13359d92a3f
James Williams <jamesrwilliams91@gmail.com>
2015-05-08 09:12:46 -0700

    update client.rb logic to include finding and updating info

--- lib/client.rb
+++ lib/client.rb
@@ -30,7 +30,20 @@ class Client
       self.name == another_client.name && self.id == another_client.id
     end
 
+    def self.find(id)
+        client = DB.exec("SELECT * FROM clients WHERE id = #{id} ;")
+        name = client.first['name']
+        stylist_id = client.first['stylist_id']
+        Client.new(name: name, id: id, stylist_id: stylist_id)
+    end
 
+    def update(attributes)
+      @name = attributes.fetch(:name, @name)
+      DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{self.id} ;")
+
+      @stylist_id = attributes.fetch(:stylist_id, @stylist_id)
+      DB.exec("UPDATE clients SET stylist_id = #{@stylist_id} WHERE id = #{self.id} ;")
+    end
 
 
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -29,6 +29,31 @@ describe(Client) do
     end
   end
 
+  describe('.find') do
+      it('finds a client in the database by id') do
+        client = Client.new(name: "john smith", id: 1, stylist_id: 1)
+        client1 = Client.new(name: "john smith", id: 1, stylist_id: 1)
+        client.save
+        client1.save
+        expect(Client.find(client.id)).to(eq(client))
+      end
+    end
+
+    describe('#update') do
+      it('allows the client name to be changed or updated') do
+        client = Client.new(name: "john smith", id: 1, stylist_id: 1)
+        client.save
+        client.update(name: "fred smith")
+        expect(client.name).to(eq("fred smith"))
+      end
+
+      it("allows the client stylist's id to be updated") do
+        client = Client.new(name: "john smith", id: 1, stylist_id: 1)
+        client.save
+        client.update(stylist_id: 2)
+        expect(client.stylist_id).to(eq(2))
+      end
+    end
 
 
 
