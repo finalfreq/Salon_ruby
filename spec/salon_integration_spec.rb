b8f46dc295e1118182f3f6f6ddfd956211562a64
James Williams <jamesrwilliams91@gmail.com>
2015-05-08 14:24:59 -0700

    updated capybara integration tests to test routing

--- spec/salon_integration_spec.rb
+++ spec/salon_integration_spec.rb
@@ -21,4 +21,37 @@ describe('the add a stylist process', type: :feature) do
   end
 end
 
-describe('stylist individual info')
+describe('get stylist individual info', type: :feature) do
+  it('goes to page with stylist info') do
+    stylist = Stylist.new(name: "dolly parton", id: 1)
+    stylist.save
+    visit('/stylists')
+    click_link('dolly parton')
+    expect(page).to have_content('Welcome to dolly parton page!')
+  end
+end
+
+describe('add client route', type: :feature) do
+  it('goes to page to add client info') do
+    stylist = Stylist.new(name: "dolly parton", id: 1)
+    stylist.save
+    visit('/')
+    click_link('clients page')
+    fill_in('name', with: 'joe smoe')
+    select('dolly parton', from: 'stylist_id')
+    click_button('Add Client')
+    expect(page).to have_content("joe smoe")
+  end
+end
+
+describe('visit client info page', type: :feature) do
+  it('goes to page for individual client info') do
+    stylist = Stylist.new(name: "dolly parton", id: 1)
+    stylist.save
+    client = Client.new(name: "joe smoe", id: 1, stylist_id: stylist.id)
+    client.save
+    visit('/clients')
+    click_link('joe smoe')
+    expect(page).to have_content("joe smoe")
+  end
+end
