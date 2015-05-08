a151b8b07d55ba8f70ae924b905217e4f3fcce34
James Williams <jamesrwilliams91@gmail.com>
2015-05-08 09:17:16 -0700

    update stylist logic to have ability to save and show all stylists

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -7,3 +7,19 @@ class Stylist
     @id = attributes[:id]
 
   end
+
+  def save()
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+  def self.all()
+    stylists = []
+    returned_stylists = DB.exec("SELECT * FROM stylists")
+    returned_stylists.each() do |stylist|
+      name = stylist['name']
+      id = stylist['id'].to_i
+      stylists.push(Stylist.new(name: name, id: id))
+    end
+    stylists
+  en
